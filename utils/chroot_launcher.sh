6a44b8c3a9f2c9ab47b17b2bfdf24b36907a8ea8
h8d13 <hadrien.loge@gmail.com>
2025-06-02 23:17:41 +0200

    eSpdoD 23:17 #004

--- utils/chroot_launcher.sh
+++ utils/chroot_launcher.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ALPF_DIR="$SCRIPT_DIR/../alpinestein"
 ROOT_DIR="$ALPF_DIR/root"
 PRO_D_DIR="$ALPF_DIR/etc/profile.d"
+## Host
+ASS_DIR="$SCRIPT_DIR/../assets"
 MODS_DIR="$SCRIPT_DIR/../assets/mods"
 
 # Cleanup function for this namespace
@@ -25,12 +27,12 @@ mkdir -p "$ROOT_DIR" \
     "$PRO_D_DIR"
 
 # Configure the chroot environment
-cp "$SCRIPT_DIR/../assets/config.conf" "$ROOT_DIR/.ashrc"
-chmod +x "$SCRIPT_DIR/../assets/profile.sh" && "$SCRIPT_DIR/../assets/profile.sh" "$ROOT_DIR"
+cp "$ASS_DIR/config.conf" "$ROOT_DIR/.ashrc"
+chmod +x "$ASS_DIR/profile.sh" && "$ASS_DIR/profile.sh" "$ROOT_DIR"
 cp /etc/resolv.conf "$ALPF_DIR/etc/resolv.conf"
 
 # Setup profile scripts
-cat "$SCRIPT_DIR/../assets/issue.ceauron" > "$PRO_D_DIR/logo.sh" && chmod +x "$PRO_D_DIR/logo.sh"
+cat "$ASS_DIR/issue.ceauron" > "$PRO_D_DIR/logo.sh" && chmod +x "$PRO_D_DIR/logo.sh"
 
 cp "$MODS_DIR/welcome.sh" "$PRO_D_DIR/welcome.sh" && chmod +x "$PRO_D_DIR/welcome.sh"
 cp "$MODS_DIR/version.sh" "$PRO_D_DIR/version.sh" && chmod +x "$PRO_D_DIR/version.sh"
